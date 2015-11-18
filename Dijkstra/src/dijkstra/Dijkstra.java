83f4eb004bde31a8ad83e29a988020ed54827e05
Pierre Jimenez <pjime032@jccl-08.ad.cs.fiu.edu>
2015-11-18 11:13:17 -0500

    Coded up to Graph traversal

--- Dijkstra/src/dijkstra/Dijkstra.java
+++ Dijkstra/src/dijkstra/Dijkstra.java
@@ -5,6 +5,13 @@
  */
 package dijkstra;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 /**
  *
  * @author pieca
@@ -14,34 +21,43 @@ public class Dijkstra {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        class AdjEle{
+            private int vert;
+            private int dist;
+            
+            AdjEle(int vert, int dist){
+                this.vert = vert;
+                this.dist = dist;
+            }
+            
+            int getVert(){
+                return vert;
+            }     
+            int getDist(){
+                return dist;
+            }
+            
+        }
         class Node implements Comparable {
             private int vert;
-            private String color;
             private int dist;
             private int parent;
             
-            Node(int key, String val){
+            Node(int key){
                 this.vert = key;
-                this.color = val;
                 this.dist = Integer.MAX_VALUE;
                 this.parent = 0;
             }
             
             int getVert(){
                 return vert;
-            }
-            String getColor(){
-                return color;
-            }            
+            }     
             int getDist(){
                 return dist;
             }
             int getParent(){
                 return parent;
-            }
-            void setColor(String color){
-                this.color = color;
             }
             void setDist(int dist){
                 this.dist = dist;
@@ -52,7 +68,62 @@ public class Dijkstra {
 
             @Override
             public int compareTo(Object o) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                Node temp = (Node) o;
+                if (dist > temp.getDist())
+                    return 1;
+                else if (dist == temp.getDist())
+                    return 0;
+                else
+                    return -1;
+            }
+        }
+        
+        System.out.print("STDIN: ");
+        //Scanner scan = new Scanner(System.in);   
+        Scanner scan = new Scanner(new File("SampleInput.txt"));
+        int numGraphs = scan.nextInt();
+        for(int i=1; i <= numGraphs; i++)
+        {
+            int numVerts = scan.nextInt();
+            int numEdges = scan.nextInt();
+            ArrayList[] adjList = new ArrayList[numVerts+1];
+            ArrayList visitedNodes = new ArrayList();
+            Node[] nodes = new Node[numVerts+1];
+            PriorityQueue<Node> pQueue = new PriorityQueue(numVerts);
+            
+            //Populate adjacency list with linked lists
+            for(int j=1; j <= numVerts; j++)
+            {
+                ArrayList adjVerts = new ArrayList();
+                adjList[j] = adjVerts;
+                nodes[j] = new Node(j);
+            }
+            
+            // Create adjacency List with all nodes initilized to white
+            for(int j=1; j <= numEdges; j++)
+            {
+                int x = scan.nextInt();
+                int y = scan.nextInt();
+                int dist = scan.nextInt();
+                
+                
+                adjList[x].add(new AdjEle(y, dist));
+                adjList[y].add(new AdjEle(x, dist));
+            }
+            
+            int startNode = scan.nextInt();
+            nodes[startNode].setDist(0);            
+            // Add all the nodes into the priority queue
+            for(int j =1; j <= numVerts; j++)
+            {
+                pQueue.add(nodes[j]);
+            }
+            
+            //Start traversing shortest paths
+            while(!pQueue.isEmpty())
+            {
+                Node curr = pQueue.poll();
+                visitedNodes.add(curr);
             }
         }
     }
